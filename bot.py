4ec01859b3873281902c686d59383b6888584a43
sagadav <artursagadeev97@gmail.com>
2023-05-10 00:07:18 +0500

    gghh

--- bot.py
+++ bot.py
@@ -18,30 +18,33 @@ message_id = {}
 print("hello")
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     chat_id = update.effective_chat.id
-    user_parent_msg_id = message_id[chat_id] if chat_id in message_id else ""
+    # user_parent_msg_id = message_id[chat_id] if chat_id in message_id else ""
     msg = await update.message.reply_text("...")
-    req = usesless.Completion.create(
-        prompt=update.message.text,
-        parentMessageId=user_parent_msg_id)
-    message_id[update.effective_chat.id] = req["id"]
+    # req = usesless.Completion.create(
+    #     prompt=update.message.text,
+    #     parentMessageId=user_parent_msg_id)
+    # message_id[update.effective_chat.id] = req["id"]
     # print(update.message.text)
 
-    msg = await msg.edit_text(req['text'])
     text = ""
     i = 0
-    # result = theb.Completion.create(
-    #     update.message.text,
-    #     parent_message_id=message_id[update.effective_chat.id] if update.effective_chat.id in message_id else ""
-    # )
-    # message_id[update.effective_chat.id] = next(result)['id']
-    # for data in result:
-    #     text += data['delta']
-    #     if i % 20 == 0:
-    #         try:
-    #             msg = await msg.edit_text(text)
-    #         except Exception:
-    #             print("Error = " + Exception)
-    #     i += 1
+    result = theb.Completion.create(
+        update.message.text,
+        parent_message_id=message_id[update.effective_chat.id] if update.effective_chat.id in message_id else ""
+    )
+    first = next(result)
+    message_id[update.effective_chat.id] = first['id']
+    text += first['delta']
+    for data in result:
+        text += data['delta']
+        # if i % 20 == 0:
+        #     try:
+        #         msg = await msg.edit_text(text)
+        #     except Exception:
+        #         print("Error = " + Exception)
+        # i += 1
+    msg = await msg.edit_text(text)
+    
     # if len(msg.text) != len(text):
     #     try:
     #         await msg.edit_text(text)
