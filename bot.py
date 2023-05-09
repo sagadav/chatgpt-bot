8398b1ca3cefd698d9aa86e24ad9020486f8d93e
sagadav <artursagadeev97@gmail.com>
2023-05-10 00:03:24 +0500

    gghh

--- bot.py
+++ bot.py
@@ -17,35 +17,36 @@ message_id = {}
 
 print("hello")
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    user_id = update.effective_user.id
-    # user_parent_msg_id = message_id[user_id] if user_id in message_id else ""
-    # req = usesless.Completion.create(
-    #     prompt=update.message.text,
-    #     parentMessageId=user_parent_msg_id)
-    # message_id[update.effective_user.id] = req["id"]
-    print(update.message.text)
-
+    chat_id = update.effective_chat.id
+    user_parent_msg_id = message_id[chat_id] if chat_id in message_id else ""
     msg = await update.message.reply_text("...")
+    req = usesless.Completion.create(
+        prompt=update.message.text,
+        parentMessageId=user_parent_msg_id)
+    message_id[update.effective_chat.id] = req["id"]
+    # print(update.message.text)
+
+    msg = await msg.edit_text(req['text'])
     text = ""
     i = 0
-    result = theb.Completion.create(
-        update.message.text,
-        parent_message_id=message_id[update.effective_chat.id] if update.effective_chat.id in message_id else ""
-    )
-    message_id[update.effective_chat.id] = next(result)['id']
-    for data in result:
-        text += data['delta']
-        if i % 20 == 0:
-            try:
-                msg = await msg.edit_text(text)
-            except Exception:
-                print("Error = " + Exception)
-        i += 1
-    if len(msg.text) != len(text):
-        try:
-            await msg.edit_text(text)
-        except Exception:
-            print("text: ", text, "prev: ", msg.text)
+    # result = theb.Completion.create(
+    #     update.message.text,
+    #     parent_message_id=message_id[update.effective_chat.id] if update.effective_chat.id in message_id else ""
+    # )
+    # message_id[update.effective_chat.id] = next(result)['id']
+    # for data in result:
+    #     text += data['delta']
+    #     if i % 20 == 0:
+    #         try:
+    #             msg = await msg.edit_text(text)
+    #         except Exception:
+    #             print("Error = " + Exception)
+    #     i += 1
+    # if len(msg.text) != len(text):
+    #     try:
+    #         await msg.edit_text(text)
+    #     except Exception:
+    #         print("text: ", text, "prev: ", msg.text)
 
 
 if __name__ == "__main__":
